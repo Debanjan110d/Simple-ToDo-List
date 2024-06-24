bf1ad1758531cbe788d1cf4e0ba6a30496d797b3
Debanjan110d <debanjangamedu@gmail.com>
2024-06-24 23:34:35 +0530

    Refresh Problem Solved

--- script.js
+++ script.js
@@ -15,15 +15,36 @@
 
 let btn = document.getElementById("btn");
 let listGroup = document.getElementById("list-group");
-
+let todoList = [];
+let todo = "";
+let description = "";
+let newListItem = "";
+let todoListHtml = "";
+let todoListStorage = JSON.parse(localStorage.getItem("todoList"));
+if (todoListStorage) {
+	todoList = todoListStorage;
+	todoList.forEach((todo) => {
+		newListItem = `<li>${todo.title}: ${todo.description}</li>`;
+		todoListHtml += newListItem;
+	});
+	listGroup.innerHTML = todoListHtml;
+}
 btn.onclick = function () {
-	let todo = document.getElementById("title").value;
-	let description = document.getElementById("description").value;
-	localStorage.setItem(todo, description);
-
-	let newListItem = `<li>${todo}: ${localStorage.getItem(todo)}</li>`;
+	todo = document.getElementById("title").value;
+	description = document.getElementById("description").value;
+	todoList.push({ title: todo, description: description });
+	localStorage.setItem("todoList", JSON.stringify(todoList));
+	newListItem = `<li>${todo}: ${description}</li>`;
 	listGroup.innerHTML += newListItem;
-
-	console.log(localStorage.getItem(todo));
-	console.log(`key is ${todo}`);
 };
+// btn.onclick = function () {
+// 	let todo = document.getElementById("title").value;
+// 	let description = document.getElementById("description").value;
+// 	localStorage.setItem(todo, description);
+
+// 	let newListItem = `<li>${todo}: ${localStorage.getItem(todo)}</li>`;
+// 	listGroup.innerHTML += newListItem;
+
+// 	console.log(localStorage.getItem(todo));
+// 	console.log(`key is ${todo}`);
+// };
